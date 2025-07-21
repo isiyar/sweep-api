1eae149285722a4089e48dd4d254a2c5409791ec
isiyar <aremijplohih@gmail.com>
2025-07-21 21:25:13 +0300

    feat: Add logging and CORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { IndexerModule } from "./indexer/indexer.module";
 import { ConfigModule } from "@nestjs/config";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { LoggingInterceptor } from "./interceptors/logging.interceptor";
 
 @Module({
   imports: [IndexerModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/interceptors/logging.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from "@nestjs/common";
+import { Observable, tap } from "rxjs";
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(LoggingInterceptor.name);
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const request = context.switchToHttp().getRequest();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { method, url } = request;
+    const now = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        const responseTime = Date.now() - now;
+        this.logger.log(`${method} ${url} - ${responseTime}ms`);
+      }),
+    );
+  }
+}
